a8e32f8a02ff4cdcbb96e97a9f8c5ded709868c6
angela-moon <angelamoon2446@gmail.com>
2020-06-18 16:26:08 -0400

    Creat ch_7.py

---
+++ ch_7.py
@@ -0,0 +1,31 @@
+# CRASH COURSE PYTHON
+# Chapter 7: User Input and While Loops
+
+# 7-1 Rental Car
+
+
+# 7-2 Restaurant Seating
+
+
+# 7-3 Multiples of Ten
+
+
+# 7-4 Pizza Toppings
+
+
+# 7-5 Movie Tickets
+
+
+# 7-6 Three Exits
+
+
+# 7-7 Infinity
+
+
+# 7-8 Deli
+
+
+# 7-9 No Pastrami
+
+
+# 7-10 Dream Vacation
