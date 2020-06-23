f8dea0a6c2820edeb0a96e97efadac98a3d71b25
angela-moon <angelamoon2446@gmail.com>
2020-06-23 14:46:14 -0400

    Create ch_8.py

---
+++ ch_8.py
@@ -0,0 +1,52 @@
+# PYTHON CRASH COURSE
+# Chapter 8: FUnctions
+
+# 8-1 Message
+
+
+# 8-2 Favorite Book
+
+
+# 8-3 T-Shirt
+
+
+# 8-4 Large Shirts
+
+
+# 8-5 Cities
+
+
+# 8-6 City Names
+
+
+# 8-7 Album
+
+
+# 8-8 User Albums
+
+
+# 8-9 Messages
+
+
+# 8-10 Sending Messages
+
+
+# 8-11 Archived Messages
+
+
+# 8-12 Sandwiches
+
+
+# 8-13 User Profile
+
+
+# 8-14 Cars
+
+
+# 8-15 Printing Models
+
+
+# 8-16 Imports
+
+
+# 8-17 Styling Functions
