d83b3f5e6f591c32e2a4ea188b899e5717c18a28
angela-moon <angelamoon2446@gmail.com>
2020-06-25 22:02:22 -0400

    8-1 to 8-4

--- ch_8.py
+++ ch_8.py
@@ -2,16 +2,31 @@
 # Chapter 8: FUnctions
 
 # 8-1 Message
+def display_message():
+    print("i am learning lots, trust me")
 
+display_message()
 
 # 8-2 Favorite Book
+def favorite_book(title):
+    print(f"One of my favorite books is {title.title()}.")
 
+favorite_book("the giver")
 
 # 8-3 T-Shirt
+def make_shirt(size, message):
+    print(f"The size of your t-shirt is: {size.title()}.")
+    print(f"The message on your t-shirt is: '{message}'.")
 
+make_shirt("small", "wow i have a cool shirt")
+make_shirt(message="yes this is a cool shirt", size="medium")
 
 # 8-4 Large Shirts
-
+def make_shirt2(size="Large", message="I love Python"):
+    print(f"You have ordered a {size} t-shirt with the message '{message}'.")
+make_shirt2()
+make_shirt2(size="Medium")
+make_shirt2(size="Small", message="I love Java")
 
 # 8-5 Cities
 
