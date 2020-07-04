404eb37d518142cde30c3b894c3f36df7b52a778
angela-moon <angelamoon2446@gmail.com>
2020-07-04 08:11:47 -0400

    11-3

--- ch_11_tests.py
+++ ch_11_tests.py
@@ -8,8 +8,15 @@ class CCTestCase(unittest.TestCase):
         self.assertEqual(formatted_city, 'Boston, Massachusetts - population 50000')
 
     def setUp(self):
-        self.
-    def (self):
+        self.employee1 = Employee("Jim", "Halpert", 90000, "Dunder Mifflin Paper Company")
+
+    def test_give_default_raise(self):
+        self.employee1.give_raise()
+        self.assertEqual(self.employee1.annual_salary, 95000)
+
+    def test_give_custom_raise(self):
+        self.employee1.give_raise(5000000)
+        self.assertEqual(self.employee1.annual_salary, 5090000)
 
 
 if __name__ == '__main__':
